30a12586744db41293240156fdc85b481c46dad2
Andrew Luxmore <andrew.luxmore@durhamcollege.ca>
2025-05-23 12:44:32 -0400

    Initial commit

---
+++ test/ecs-bluegreen.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsBluegreen from '../lib/ecs-bluegreen-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-bluegreen-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsBluegreen.EcsBluegreenStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
