3c060c047db38fd14927b8c33a05ce3ce712b035
Andrew Luxmore <andrew.luxmore@durhamcollege.ca>
2025-05-23 13:56:02 -0400

    trying to get blue green

--- bin/ecs-bluegreen.ts
+++ bin/ecs-bluegreen.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { EcsBluegreenStack } from '../lib/ecs-bluegreen-stack';
+import { EcsBlueGreenStack } from '../lib/ecs-bluegreen-stack';
 
 const app = new cdk.App();
-new EcsBluegreenStack(app, 'EcsBluegreenStack', {
+new EcsBlueGreenStack(app, 'EcsBlueGreenStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/ecs-bluegreen-stack.ts
+++ lib/ecs-bluegreen-stack.ts
@@ -1,16 +1,199 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, Duration, CfnOutput, SecretValue } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
-export class EcsBluegreenStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+const repositoryName = 'sample-container-app';
+const gitHubOwner = 'arluxmore';
+
+export class EcsBlueGreenStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const region = Stack.of(this).region;
+    const account = Stack.of(this).account;
+    
+    const allowedIp = this.node.tryGetContext('allowedIp') ?? '0.0.0.0/0';
+    const imageTag = this.node.tryGetContext('imageTag');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsBluegreenQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    if (imageTag === undefined) {
+      throw new Error('image tag required - use nginx for first deploy');
+    }
+
+    const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 2 });
+
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+    
+    const repo = new ecr.Repository(this, 'AppRepo');
+
+    const taskDef = {
+      cpu: 256,
+      memoryLimitMiB: 512,
+      executionRole: new iam.Role(this, 'FargateExecutionRole', {
+        assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'),
+        ],
+      }),
+    };
+
+    // Task Definition (Blue - Nginx)
+    const blueTaskDef = new ecs.FargateTaskDefinition(this, 'BlueTaskDef', taskDef);
+    const greenTaskDef = new ecs.FargateTaskDefinition(this, 'GreenTaskDef', taskDef);    
+
+    if (imageTag === 'nginx') {
+      blueTaskDef.addContainer('BlueApp', {
+        portMappings: [{ containerPort: 80 }],
+        image: ecs.ContainerImage.fromRegistry('nginx:alpine'),
+      });
+    } else {
+      blueTaskDef.addContainer('BlueApp', {
+        portMappings: [{ containerPort: 80 }],
+        image: ecs.ContainerImage.fromRegistry(`${account}.dkr.ecr.${region}.amazonaws.com/${repositoryName}:${imageTag}`),
+      });
+    }
+
+    greenTaskDef.addContainer('GreenApp', {
+      image: ecs.ContainerImage.fromRegistry('nginx:alpine'),
+      portMappings: [{ containerPort: 80 }],
+    });
+
+    // Fargate Service (Blue)
+    const blueService = new ecs.FargateService(this, 'BlueService', {
+      cluster,
+      taskDefinition: blueTaskDef,
+      desiredCount: 1,
+      assignPublicIp: true,
+    });
+
+    const greenService = new ecs.FargateService(this, 'GreenService', {
+      cluster,
+      taskDefinition: greenTaskDef,
+      desiredCount: 1,
+      assignPublicIp: true,
+      deploymentController: {
+        type: ecs.DeploymentControllerType.CODE_DEPLOY,
+      },
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true,
+    });
+
+    const listener = lb.addListener('HttpListener', {
+      port: 80,
+      open: true,
+    });
+
+    // Target Groups
+    const targetGroup = {
+      vpc,
+      port: 80,
+      targetType: elbv2.TargetType.IP,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      healthCheck: { path: '/' },
+    };
+
+    const blueTG = new elbv2.ApplicationTargetGroup(this, 'BlueTG', targetGroup);
+    const greenTG = new elbv2.ApplicationTargetGroup(this, 'GreenTG', targetGroup);
+
+    listener.addTargetGroups('DefaultRule', {
+      targetGroups: [blueTG],
+    });
+
+    listener.addTargetGroups('GreenRule', {
+      priority: 10, // must be unique and > 1 (default has lowest priority)
+      conditions: [
+        elbv2.ListenerCondition.pathPatterns(['/green']),
+        elbv2.ListenerCondition.sourceIps([allowedIp]),
+      ],
+      targetGroups: [greenTG],
+    });
+
+    blueService.attachToApplicationTargetGroup(blueTG);
+    greenService.attachToApplicationTargetGroup(greenTG);
+
+    // CodeBuild Project
+    const project = new codebuild.PipelineProject(this, 'GreenBuildProject', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true, // required for Docker
+      },
+      environmentVariables: {
+        REPOSITORY_URI: { value: repo.repositoryUri },
+        IMAGE_TAG: { value: 'latest' },
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          pre_build: {
+            commands: [
+              'echo Logging in to Amazon ECR...',
+              'aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $REPOSITORY_URI',
+            ],
+          },
+          build: {
+            commands: [
+              'echo Building the Docker image...',
+              'docker build -t $REPOSITORY_URI:$IMAGE_TAG .',
+              'docker push $REPOSITORY_URI:$IMAGE_TAG',
+            ],
+          },
+        },
+      }),
+    });
+
+    repo.grantPullPush(project.role!);
+
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+
+    const pipeline = new codepipeline.Pipeline(this, 'GreenPipeline', {
+      pipelineName: 'GreenDeployPipeline',
+    });
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: 'GitHub_Source',
+          oauthToken: SecretValue.secretsManager('github-token'),
+          owner: gitHubOwner,
+          repo: repositoryName,
+          output: sourceOutput,
+          branch: 'main',
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: 'BuildAndPush',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Build_and_Push_Image',
+          project,
+          input: sourceOutput,
+          outputs: [buildOutput],
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: 'DeployGreen',
+      actions: [
+        new codepipeline_actions.EcsDeployAction({
+          actionName: 'DeployToGreen',
+          service: greenService,
+          input: buildOutput,
+        }),
+      ],
+    });
   }
 }
