6e46bdf43c992e31a034376c2565d009595eeff5
Andrew Luxmore <andrew.luxmore@durhamcollege.ca>
2025-05-23 22:12:33 -0400

    port thing

--- lib/ecs-bluegreen-stack.ts
+++ lib/ecs-bluegreen-stack.ts
@@ -62,13 +62,21 @@ export class EcsBlueGreenStack extends Stack {
 
     greenTaskDef.addContainer('GreenApp', {
       image: ecs.ContainerImage.fromRegistry('nginx:alpine'),
-      portMappings: [],
+      portMappings: [{
+        containerPort: 80,
+        hostPort: 0,
+        protocol: ecs.Protocol.TCP,
+      }],
     });
 
     // Proxy container
     greenTaskDef.addContainer('Proxy', {
       image: ecs.ContainerImage.fromAsset('./proxy'),
-      portMappings: [{ containerPort: 80 }],
+      portMappings: [{
+        containerPort: 80, // proxy listens on 80 inside the container
+        hostPort: 80,      // maps to port 80 on the ECS host for ALB
+        protocol: ecs.Protocol.TCP,
+      }],
       essential: true,
     });
 
